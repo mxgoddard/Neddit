143a2250ea4def1a03b6f06ab6bf19aa6786579c
Max Goddard <maxolivergoddard@gmail.com>
2018-11-12 18:42:09 +0000

    Added postComment axios post request

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,4 +5,15 @@ const BASE_URL = 'https://ncreddit.herokuapp.com/api/';
 export const getArticles = async () => {
     const { data } = await axios.get(`${BASE_URL}/articles`);
     return data.articles;
-}
+};
+
+export const getArticle = async (id) => {
+    const { data } = await axios.get(`${BASE_URL}/articles/${id}`);
+    return data.article;
+};
+
+export const postComment = async (comment, id) => {
+    const url = `${BASE_URL}articles/${id}/comments`;
+    const { data } = await axios.post(url, comment);
+    return data;
+};
