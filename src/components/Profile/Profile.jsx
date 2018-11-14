6b74ecc1855f1c4ecf9e98e191903ee598911434
Max Goddard <maxolivergoddard@gmail.com>
2018-11-14 17:10:43 +0000

    Added profile page with ability to logout

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { navigate } from '@reach/router';
+
+class Profile extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Profile Page!</h2>
+
+                <button onClick={this.handleClick}>Logout</button>
+            </div>
+        );
+    };
+
+    handleClick = () => {
+        // Logout
+        localStorage.removeItem("user");
+        localStorage.removeItem("userID");
+        this.props.login('Login');
+        navigate('/');
+    }
+};
+
+export default Profile;
