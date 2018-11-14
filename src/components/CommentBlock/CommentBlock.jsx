ed8b9e4b80702f052f3bafddd35c7731550859d2
Max Goddard <maxolivergoddard@gmail.com>
2018-11-14 09:52:10 +0000

    Formatted comments to look nicerm working on implementing functionality

--- src/components/CommentBlock/CommentBlock.jsx
+++ src/components/CommentBlock/CommentBlock.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './CommentBlock.css';
 
-const CommentBlock = (props) => {
-    return (
-        <div className="CommentBlock">
-             {console.log(props)}
-            <p>{props.data.body}</p>
+class CommentBlock extends Component {
+    render() {
+
+        const { created_by, votes, body } = this.props.data;
+        
+        return (
+            <div className="CommentBlock">
+                {console.log(this.props.data)}
+            <div onClick={this.likeComment} className="LikeCommentDiv">
+
+            </div>
+            <div className="AboutCommentDiv">
+                <p id="createdByUser">{ created_by.username } </p> <p>  · { votes } points</p>
+            </div>
+            <div className="CommentContentDiv">
+                <p>{ body }</p>
+            </div>
         </div>
-    );
+        );
+    };
+
+    likeComment = () => {
+        console.log('Liked comment');
+    };
 };
 
 export default CommentBlock;
